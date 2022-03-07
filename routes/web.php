83e823dd153e50bf395d7e9a017eac44d05bc13c
soufiene ait ali <soufiene.aitali@gmail.com>
2022-03-07 16:15:45 +0100

    first day

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
+Auth::routes();
+
 Route::get('/{page}', 'AdminController@index');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
